f8e336babcf003780e1ce25c6a90a565ddcc2250
chan <kimynam1@naver.com>
2022-01-08 22:38:26 +0900

    redux  todo

--- src/index.js
+++ src/index.js
@@ -7,26 +7,64 @@ const todoUl = document.querySelectorAll('.todo-list')[0];
 
 log(abbBtn, input, todoUl);
 
-const todoDelete = (e) => {
-  if(e.target.className === 'remove-btn'){
-    const pane = e.target.parentNode;
-    pane.remove();
+const ADD = 'ADD';
+const DELETE = 'DELETE';
+const initalState = [];
+//redux 적용
+
+const todoReducer = (state = initalState, action) => {
+  log(state,'state',action ,'action')
+  switch(action.type){
+    case ADD :
+      return [
+        ...state,
+        {
+          id:state.length,
+          title:action.title,
+        }
+      ]
+    case DELETE :
+      return state.filter(item => item.id !== action.id);
+    default : return state;
   }
 }
-const todoAdd = () => {
-  const { value } = input;
-  if(value.length < 1) return alert('입력해주세요!')
+const onChnage = () => {
+  const todo = todoStore.getState();
   const li  = document.createElement('li');
   const deleteBtn = document.createElement('button');
   deleteBtn.classList.add('remove-btn')
   deleteBtn.innerText = '❌';
-  li.innerText = value;
-  li.append(deleteBtn);
-  todoUl.append(li);
-  input.value = '';
+  todo.map((item, idx) => {
+    li.innerText = item.title;
+    li.append(deleteBtn);
+    deleteBtn.addEventListener('click',(e)=> todoDelete(e,idx))
+    todoUl.append(li);
+  })
+}
+const todoStore = createStore(todoReducer);
+const todoAdd = () => {
+  todoStore.dispatch({
+    type:ADD,
+    title:input.value,
+  })
+}
+
+const todoDelete = (e, idx) => {
+  if(e.target.className === 'remove-btn'){
+    const pane = e.target.parentNode;
+    pane.remove();
+    todoStore.dispatch({
+      type:DELETE,
+      id:idx
+    })
+  }
 }
 
 
+
+todoStore.subscribe(onChnage);
 abbBtn.addEventListener('click', todoAdd);
 todoUl.addEventListener('click', todoDelete);
-const intalalState = [];
+
+
+console.log(todoStore ,'todoStore')
